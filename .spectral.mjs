6021046f540fed1c7ea78da92020825d71ae7f8d
Joo-Kwang Park <iam@jkpark.me>
2023-09-12 18:58:08 +0900

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxODMwMw/spectral.js?branch_slug=main&token=134e6d53-40ca-4cb8-8092-51f582bc980c";
+export default { extends: ruleset };
